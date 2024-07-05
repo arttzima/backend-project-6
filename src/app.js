9a002bd7d54fff456d8b05caaea21205d92a2270
Artem Taran <artt.zima@protonmail.com>
2024-07-05 11:16:48 +0300

    Changed default localhost address for running on Render

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import fastify from 'fastify';
 
 const app = fastify();
 const port = 3000;
+const host = ('RENDER' in process.env) ? '0.0.0.0' : 'localhost';
 
-app.listen({ port }, () => {
+app.listen({ host, port }, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
 app.get('/', (req, res) => {
-  res.send('World!');
+  res.send('The app is runnig!');
 });
 
 export default () => app;
